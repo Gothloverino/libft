a3c0a9c15f357493ec2657d72c6bb3ecac9b9c03
Alessandro Signoroni <asignoro@c1r6p10.42firenze.it>
2024-11-26 12:40:32 +0100

    wip

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,28 +6,28 @@
 /*   By: asignoro <asignoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 10:57:11 by asignoro          #+#    #+#             */
-/*   Updated: 2024/11/26 11:37:02 by asignoro         ###   ########.fr       */
+/*   Updated: 2024/11/26 12:39:36 by asignoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-       #include <string.h>
+#include "libft.h"
 
-       void *ft_memmove(void *dest, const void *src, size_t n)
-       {
-        size_t i;
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+size_t i;
 
-        if (dest > src)
-        {
-            i = n;
-            while(i > 0)
-            {
-                i--;
-                ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-            }
-        }
-        else
-        {
-            ft_memcpy(dest, src, n);
-        }
-        return (dest);
-       }
+if (dest > src)
+{
+    i = n;
+    while(i > 0)
+    {
+        i--;
+        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+    }
+}
+else
+{
+    ft_memcpy(dest, src, n);
+}
+return (dest);
+}
